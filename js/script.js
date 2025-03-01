2b99089afd83dff4e3107db6f376afeced2bd728
francescovalenti <francescovalenti83@outlook.it>
2025-03-01 20:11:59 +0100

    feat:5

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 const datiutenti = document.getElementById ("dati-utenti");
 console.log(datiutenti);
+const etainput = document.getElementById ("eta")
+console.log (etainput)
+const kminput = document.getElementById ("km")
+console.log( kminput);
+
