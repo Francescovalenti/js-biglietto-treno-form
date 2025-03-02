381f7e3189b26f382ac27964ebe84bc992e23ae7
francescovalenti <francescovalenti83@outlook.it>
2025-03-02 18:38:04 +0100

    feat:7

--- js/script.js
+++ js/script.js
@@ -1,28 +1,43 @@
-const etainput = (document.getElementById ("eta"));
-const kminput =  (document.getElementById("km"));
+const datiutenti = document.getElementById("datiutenti");
+console.log(datiutenti);
+const inputetastr = (document.getElementById("inputeta"));
+const inputkmstr = (document.getElementById("inputkm"));
+let km = parseInt(inputkmstr);
+let eta = parseInt(inputetastr);
 const prezzokm = 0.21;
-let prezzoTotale = km * prezzokm;
+let prezzoTotale = inputkm * prezzokm;
+console.log(inputetastr, inputkmstr);
 
 
-document.getElementById("calcola")
-calcola.addEventListener(`Click`, function () {
-    const eta= etainput.Value;
-    const km= kminput.value;
-    console.log(eta);
+document.getElementById("calcola").addEventListener(`Click`, function () {
+    const inputeta = inputeta.Value;
+    const inputkm = inputkm.value;
+   
+
+    if (isNaN(km) || isNaN(age) || km <= 0 || age <= 0) {
+        alert("Per favore, inserisci valori validi per chilometri ed età.");
+        return;
+    }
+
+
+
+    if (eta < 18) {
+        let sconto = prezzoTotale * 20 / 100;
+        prezzoTotale = prezzoTotale - sconto;
+    } else if (eta > 65) {
+        let sconto = prezzoTotale * 40 / 100;
+        prezzoTotale = prezzoTotale - sconto;
+    }
+    prezzoTotale =
+        (Math.round(prezzototale * 100) / 100).toFixed(2)
     
 
+    const risultato = document.getElementById('risultato');
+    document.getElementById("prezzo").innerText = prezzoTotale;
+    document.getElementById("risultato").style.display = "block";
+    console.log(prezzoTotale);
+   
 
-    if (eta <18){
-        let sconto = prezzoTotale * 20  / 100;
-        prezzoTotale= prezzoTotale - sconto;
-    } else if (eta > 65) {
-        let sconto = prezzoTotale * 40 /100;
-        prezzoTotale= prezzoTotale - sconto;
-    }
-    prezzoTotale= 
-    (Math.round (prezzototale * 100) / 100).toFixed(2)
-    document.getElementById("prezzo").innerText = prezzoTotale
-     document.getElementById("risultato").style.display ="block";
 
 
 })
