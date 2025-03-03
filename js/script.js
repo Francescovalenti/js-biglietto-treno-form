1d94e4ae3bf0325b11a655a82de1ef0decc53c45
francescovalenti <francescovalenti83@outlook.it>
2025-03-03 12:13:09 +0100

    feat:14

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // raccolta dati
-const datiutenti = document.getElementById("datiutenti");
+const datiutentiinput = document.getElementById("datiutenti");
 console.log(datiutenti)
+const nomeecognomeinput = document.getElementById("nomeecognome");
+console.log(nomeecognome);
 const etainput = document.getElementById("eta");
 console.log(etainput);
 const kminput = document.getElementById("km");
@@ -11,33 +13,37 @@ console.log(calcolainput);
 
 
 
+
 // processo di esecuzione
 datiutenti.addEventListener("submit", function (event) {
     event.preventDefault();
     console.log("submit");
+    const nomeecognome = (nomeecognomeinput.value);
+    console.log(nomeecognome);
     const eta = (etainput.value);
     console.log(eta);
     const km = (kminput.value);
     console.log(km);
+   
 
     const prezzobase = 0.21;
     let prezzoTotale = km * prezzobase;
 
 
-    if (eta < 18) { 
-        let sconto = prezzoTotale * 20  / 100;
-        prezzoTotale= prezzoTotale - sconto;
+    if (eta < 18) {
+        let sconto = prezzoTotale * 20 / 100;
+        prezzoTotale = prezzoTotale - sconto;
     }
     else if (eta > 65) {
-        let sconto = prezzoTotale * 20  / 100;
-        prezzoTotale= prezzoTotale - sconto;
+        let sconto = prezzoTotale * 20 / 100;
+        prezzoTotale = prezzoTotale - sconto;
     }
-    prezzoTotale=
+    prezzoTotale =
 
-(Math.round    (prezzoTotale * 100) / 100).toFixed(2);
+        (Math.round(prezzoTotale * 100) / 100).toFixed(2);
 
-console.log(prezzoTotale);
-document.getElementById("prezzo").innerHTML=`${prezzoTotale}`;
+    console.log(prezzoTotale);
+    document.getElementById("prezzo").innerHTML = `  il costo del biglietto per ${nomeecognome} è di ${prezzoTotale} € `;
 
 })
 
