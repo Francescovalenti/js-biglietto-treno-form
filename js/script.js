e878dc0c47bb6d7041242a73c6e5cdac46e4bfe7
francescovalenti <francescovalenti83@outlook.it>
2025-03-03 10:24:57 +0100

    feat:11

--- js/script.js
+++ js/script.js
@@ -1,62 +1,14 @@
+// raccolta dati
 const datiutenti = document.getElementById("datiutenti");
- console.log(datiutenti);
- const eta = document.getElementById("eta");
- console.log(eta);
- const inputkm = document.getElementById("km");
- console.log(km);
- const risultatoElement = document.getElementById("risultato");
- console.log(risultatoElement);
+console.log(datiutenti)
+const eta = document.getElementById("eta");
+console.log(eta);
+const km = document.getElementById("km");
+console.log(km);
 
-
-
-
-
-
-
-// if (isNaN(km) || isNaN(eta) || km <= 0 || eta <= 0) {
-
-// }
-
-
-document.getElementById("calcola")
-Button.addEventListener(`Click`, function () {
-    ;
-    console.log("datiutenti");
-    // Prelevare i dati dagli inputs
-    const eta = eta.value;
-    const km = km.value;
-    console.log(eta, km);
-
-    const prezzokm = 0.21;
-    let prezzoTotale = km * prezzokm;
-    console.log(prezzoTotale);
+// processo di esecuzione
+datiutenti.addEventListener("click",function () {
+    console.log("Click");
     
-
-
-
-
-
-
-
-
-    if (eta < 18) {
-        let sconto = prezzoTotale * 20 / 100;
-        prezzoTotale = prezzoTotale - sconto;
-    } else if (eta > 65) {
-        let sconto = prezzoTotale * 40 / 100;
-        prezzoTotale = prezzoTotale - sconto;
-    }
-    prezzoTotale =
-        (Math.round(prezzoTotale * 100) / 100).toFixed(2)
-    console.log(prezzoTotale);
-
-
-    const risultato = document.getElementById('risultato');
-    document.getElementById("prezzo").innerText = `<p>Il prezzo del biglietto è: ${prezzoTotale} €</p>`;
-    document.getElementById("risultato").style.display = "block";
-
-
-
-
-
 })
+
